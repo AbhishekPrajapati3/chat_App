408cc5f9a74a4cf5d3625486d553d14e731d7825
Abhishek Kumar <abprajapati3333@gmail.com>
2025-07-09 00:24:48 +0530

    integrate cometchat

---
+++ lib/cometchat_config.dart
@@ -0,0 +1,5 @@
+class CometChatConfig {
+  static const String appId = "278353c9e1c1c07e"; // Replace with your App ID
+  static const String region = "in"; // Replace with your App Region
+  static const String authKey = "13e99b721564236aaf98fa533def47004d48d472"; // Replace with your Auth Key
+}
